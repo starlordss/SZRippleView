61e0a9c66b9bf6e7eb3871eb28454cca9788517a
isstarz <isstarz@163.com>
2017-10-18 00:07:16 +0800

    设计接口&初始化ripple view

--- SZRippleView/SZRippleView/SZRippleView.h
+++ SZRippleView/SZRippleView/SZRippleView.h
@@ -8,6 +8,33 @@
 
 #import <UIKit/UIKit.h>
 
+/// 二进制表示颜色
+#define SZHexColor(hexValue) SZHexColorA(hexValue, 1.0)
+
+#define SZHexColorA(hexValue, a) [UIColor colorWithRed:((float)(((hexValue) & 0xFF0000) >> 16))/255.0 green:((float)(((hexValue) & 0xFF00) >> 8))/255.0 blue:((float)((hexValue) & 0xFF))/255.0 alpha:a]
+
+/// 一款底部水波动画的view控件
 @interface SZRippleView : UIView
+
+/** 振幅*/
+@property (nonatomic, assign) CGFloat amplitude;
+/** 周期*/
+@property (nonatomic, assign) CGFloat period;
+/** 速度*/
+@property (nonatomic, assign) CGFloat speed;
+/** 波纹的高度*/
+@property (nonatomic, assign) CGFloat waveHeight;
+/** 波纹的宽度*/
+@property (nonatomic, assign) CGFloat waveWidth;
+/** 波纹的Y值*/
+@property (nonatomic, assign) CGFloat wavePointY;
+/** 波浪x位移*/
+@property (nonatomic, assign) CGFloat waveOffsetX;
+/** 波浪颜色*/
+@property (nonatomic, strong) UIColor *waveColor;
+
+
+
++ (instancetype)rippleViewWithFrame:(CGRect)frame startColor:(UIColor *)sColor endColor:(UIColor *)eColor;
 
 @end
--- SZRippleView/SZRippleView/SZRippleView.m
+++ SZRippleView/SZRippleView/SZRippleView.m
@@ -9,14 +9,37 @@
 
 #import "SZRippleView.h"
 
+@interface SZRippleView ()
+@property (nonatomic, strong) UIColor *startColor;
+@property (nonatomic, strong) UIColor *endColor;
+@property (nonatomic, strong) CADisplayLink *displayLink;
+@property (nonatomic, strong) CAShapeLayer *shapeLayer1;
+@property (nonatomic, strong) CAShapeLayer *shapeLayer2;
+@property (nonatomic, strong) CAGradientLayer *gradientLayer1;
+@property (nonatomic, strong) CAGradientLayer *gradientLayer2;
+
+@end
+
 @implementation SZRippleView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark -initialize-
++ (instancetype)rippleViewWithFrame:(CGRect)frame startColor:(UIColor *)sColor endColor:(UIColor *)eColor
+{
+    return [[self alloc] initWithFrame:frame startColor:sColor endColor:eColor];
 }
-*/
+
+- (instancetype)initWithFrame:(CGRect)frame startColor:(UIColor *)sColor endColor:(UIColor *)eColor
+{
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        _startColor = sColor;
+        _endColor = eColor;
+        self.backgroundColor = SZHexColorA(0xedf0f4, 0.1f);
+        self.layer.masksToBounds = YES;
+    }
+    return self;
+   
+}
 
 @end
