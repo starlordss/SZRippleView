d7177e88b286147a3709bce35f8e61bd9d72c709
isstarz <isstarz@163.com>
2017-10-17 23:45:34 +0800

    init project

---
+++ SZRippleView/SZRippleView/SZRippleView.h
@@ -0,0 +1,13 @@
+//
+//  SZRippleView.h
+//  SZRippleView
+//
+//  Created by Zahi on 2017/10/17.
+//  Copyright © 2017年 Zahi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SZRippleView : UIView
+
+@end
---
+++ SZRippleView/SZRippleView/SZRippleView.m
@@ -0,0 +1,22 @@
+
+//
+//  SZRippleView.m
+//  SZRippleView
+//
+//  Created by Zahi on 2017/10/17.
+//  Copyright © 2017年 Zahi. All rights reserved.
+//
+
+#import "SZRippleView.h"
+
+@implementation SZRippleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
