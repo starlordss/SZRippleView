a4acbd4bf03346fd7bc78754c085e76167363029
isstarz <isstarz@163.com>
2017-10-17 23:41:52 +0800

    Initial Commit

---
+++ SZRippleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SZRippleView
+//
+//  Created by Zahi on 2017/10/17.
+//  Copyright © 2017年 Zahi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SZRippleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SZRippleView
+//
+//  Created by Zahi on 2017/10/17.
+//  Copyright © 2017年 Zahi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
